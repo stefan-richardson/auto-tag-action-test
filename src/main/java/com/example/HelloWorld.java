1a08a7a6cdcae862f8091d6675d2c9b51ee6f03a
Stefan Richardson <stefan.richardson@qhrtech.com>
2025-03-28 15:33:27 -0600

    feat(LOGGING-1234): testing

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Beautiful Cruel World! WHY ARE YOU BEING LIKE THIS!!??");
+        System.out.println("Hello, Beautiful Cruel World!");
     }
 }
