f79c852434dfed76238d811776442862829dcf52
Stefan Richardson <stefan.r.richardson@gmail.com>
2025-04-09 22:03:19 -0600

    feat(*)spelling

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Helloooo Nurse!");
+        System.out.println("Hello Nurse!");
     }
 }
